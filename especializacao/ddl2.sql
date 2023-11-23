3a4833eabff0888a9480fd211e0ffd41006171f8
HacKairos <joao.brasilpv@gmail.com>
2023-11-23 20:44:27 -0300

    .

--- especializacao/ddl2.sql
+++ especializacao/ddl2.sql
@@ -54,13 +54,49 @@
 -- );
 
 -- Exemplo de especialização parcial sobreposta 6
-CREATE TABLE pessoa (
+-- CREATE TABLE pessoa (
+--     id SERIAL PRIMARY KEY,
+--     nome VARCHAR(100),
+--     salario DECIMAL(10, 2),
+--     cargo VARCHAR(50),
+--     cpf VARCHAR(11),
+--     telefone VARCHAR(20),
+--     boolFuncionario boolean,
+--     boolCliente boolean
+-- );
+
+-- Exemplo de especialização total exclusiva 8
+-- CREATE TABLE entidade_controladora (
+--     id_entidade SERIAL PRIMARY KEY,
+--     tipo VARCHAR(20)
+-- );
+-- CREATE TABLE pessoa (
+--     id SERIAL PRIMARY KEY REFERENCES entidade_controladora(id_entidade),
+--     nome VARCHAR(100),
+--     salario DECIMAL(10, 2),
+--     cargo VARCHAR(50),
+--     cpf VARCHAR(11),
+--     telefone VARCHAR(20)
+-- );
+
+
+CREATE TABLE entidade_controladora (
     id SERIAL PRIMARY KEY,
+    tipo VARCHAR(20)
+);
+
+-- Exemplo de especialização total sobreposta 9
+CREATE TABLE cliente(
+    id SERIAL PRIMARY KEY REFERENCES entidade_controladora(id),
+    nome VARCHAR(100),
+    cpf VARCHAR(11),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE func(
+    id SERIAL PRIMARY KEY REFERENCES entidade_controladora(id),
     nome VARCHAR(100),
     salario DECIMAL(10, 2),
-    cargo VARCHAR(50),
-    cpf VARCHAR(11),
-    telefone VARCHAR(20),
-    boolFuncionario boolean,
-    boolCliente boolean
+    cargo VARCHAR(50)
 );
+
